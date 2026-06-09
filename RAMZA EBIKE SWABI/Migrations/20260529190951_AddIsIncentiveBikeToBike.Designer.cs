abd580623da1481dc3e8bfae86c50eea6c3410aa
Abdullah Khan <abd122410@gmail.com>
2026-06-09 10:17:28 +0500

    Migration added

--- RAMZA EBIKE SWABI/Migrations/20260529190951_AddIsIncentiveBikeToBike.Designer.cs
+++ RAMZA EBIKE SWABI/Migrations/20260529190951_AddIsIncentiveBikeToBike.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ramza_EBike_Swabi.Data;
 
@@ -11,9 +12,11 @@ using Ramza_EBike_Swabi.Data;
 namespace RAMZA_EBIKE_SWABI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260529190951_AddIsIncentiveBikeToBike")]
+    partial class AddIsIncentiveBikeToBike
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RAMZA EBIKE SWABI/Migrations/20260529190951_AddIsIncentiveBikeToBike.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RAMZA_EBIKE_SWABI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsIncentiveBikeToBike : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsIncentiveBike",
+                table: "VendorBillItem",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsIncentiveBike",
+                table: "Bikes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsIncentiveBike",
+                table: "VendorBillItem");
+
+            migrationBuilder.DropColumn(
+                name: "IsIncentiveBike",
+                table: "Bikes");
+        }
+    }
+}
